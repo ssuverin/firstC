772173691060486495e6cc8ef6ade51a632028da
ssuverin <ssuverin@gmail.com>
2022-10-21 15:21:36 +0300

    third task

---
+++ third_task/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число - ");
+int firstNumber = Int16.Parse(Console.ReadLine());
+
+if (firstNumber % 2 == 0) {
+    Console.WriteLine($"Число {firstNumber} четнное");
+}
+else {
+    Console.WriteLine($"Число {firstNumber} нечетнное");
+}
