fab5b462f306d8bc26ec4d044397b2c0d725f5aa
ssuverin <ssuverin@gmail.com>
2022-10-27 20:57:59 +0300

    Определение выходного дня

---
+++ seventh_task/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+while (true)
+{
+    Console.Write("Введите число (день недели) - ");
+    int number = Int16.Parse(Console.ReadLine());
+    if (number > 1 && number < 8) {
+        if (number == 6 || number == 7) {
+            Console.WriteLine("да");
+        }
+        else {
+            Console.WriteLine("нет");
+        }
+        break;
+    }
+}
