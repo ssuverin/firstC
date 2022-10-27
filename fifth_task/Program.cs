fea2816cdd7780fd86f21cf432b64c35a7c9ff45
ssuverin <ssuverin@gmail.com>
2022-10-27 20:18:53 +0300

    Вывод второй цифры трехзначного числа

---
+++ fifth_task/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+// выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+// Console.WriteLine("Введите трехначное число - ");
+// int number = Int16.Parse(Console.ReadLine());
+
+while (true)
+{
+    Console.WriteLine("Введите трехначное число - ");
+    int number = Int16.Parse(Console.ReadLine());
+    if (number > 99 && number < 1000)
+    {
+        int second_digit = (number / 10) % 10;
+        Console.WriteLine(second_digit);
+        break;
+    }
+}
