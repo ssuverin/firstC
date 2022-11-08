7b0377c1e06cdb702def5f21b695723fd4e46503
ssuverin <ssuverin@gmail.com>
2022-11-08 23:31:29 +0300

    Вывод суммы цифр в числе

---
+++ twelth_task/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int UserNumber(string message)
+{
+    int userNumber;
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            userNumber = number;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неправильный ввод. Повторите попытку. ");
+        }
+    }
+    return userNumber;
+}
+
+int sumNumbers(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        result = result + number % 10;
+        number = number / 10;
+    }
+    return result;
+
+}
+
+int sumNum = UserNumber("Введите число - ");
+Console.WriteLine($"Сумма цифр числа {sumNum} равна {sumNumbers(sumNum)}");
