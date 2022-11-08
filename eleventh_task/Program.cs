0168c85bc9564bfd8f1a2a7ad4a27e4b2d645a62
ssuverin <ssuverin@gmail.com>
2022-11-08 23:05:14 +0300

    Вывод степени числа в степени другого числа

---
+++ eleventh_task/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+int UserNumber(string message)
+{
+    int userNumber;
+    while (true)
+    {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            userNumber = number;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Неправильный ввод. Повторите попытку. ");
+        }
+    }
+    return userNumber;
+}
+
+double powNumbers (int x, int y) {
+    return Math.Pow(x, y);
+}
+
+int firstNumber = UserNumber("Введите первое число - ");
+int secondNumber = UserNumber("Введите второе число - ");
+
+Console.WriteLine($"{powNumbers(firstNumber, secondNumber)}");
