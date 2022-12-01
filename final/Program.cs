ccc00ceabe613e457e4b67757bf8e49d8b81f1c8
ssuverin <ssuverin@gmail.com>
2022-12-01 14:31:47 +0300

    Создание массива

--- final/Program.cs
+++ final/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите элементы массива через пробел");
+string input = Console.ReadLine();
+string [] first = input.Split(' ');
