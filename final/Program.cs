d4d0b0714703a244adc3734f8669c5e10826a70f
ssuverin <ssuverin@gmail.com>
2022-12-01 14:43:19 +0300

    Финальная сборка

--- final/Program.cs
+++ final/Program.cs
@@ -1,3 +1,17 @@
 ﻿Console.WriteLine("Введите элементы массива через пробел");
 string input = Console.ReadLine();
 string [] first = input.Split(' ');
+string [] final = new string[first.Length];
+
+var count = 0;
+for (int i = 0; i < first.Length; i++)
+{
+    if (first[i].Length <= 3) {
+        final[count] = first[i];
+        count++;
+    }
+}
+for (int j = 0; j < final.Length; j++)
+{
+    Console.Write(final[j] + ' ');
+}
